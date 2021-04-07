1749db0e8e660b616a6826798fecb269416a3f48
jonasbfranco <jonasbfranco@hotmail.com>
2021-04-06 22:35:13 -0300

    :up: PostItem

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,23 @@
 import * as React from "react"
-import { Link, link } from "gatsby"
 
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import PostItem from '../components/PostItem'
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home</h1>
+    <PostItem
+      slug="/about/"
+      background="red"
+      category="Misc"
+      date="30 de Abril de 2020"
+      timeToRead="5"
+      title="Site"
+      description="Jonas"
+      />
   </Layout>
 )
 
